3602fc08ba7adb3eaf23ce989241acb0aa0f702a
Godoy <godoydev27@gmail.com>
2023-09-19 15:56:34 -0300

    pelo amor de Deus

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function toggleMode() {
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
-
-  // substituir a imagem
+  
   if (html.classList.contains("light")) {
+  // substituir a imagem
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar2.png")
+    img.setAttribute('src', './assets/avatar2.png')
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute('src', './assets/avatar.png')
   }
 }
